111a14bc6fd0734616cbc195973ee07d3c7b1952
ashtikasingh <aashtika2002@gmail.com>
2021-08-11 18:05:32 +0530

    24th day

---
+++ 24. Content Placeholder/script.js
@@ -0,0 +1,26 @@
+const header = document.getElementById('header')
+const title = document.getElementById('title')
+const excerpt = document.getElementById('excerpt')
+const profile_img = document.getElementById('profile_img')
+const name = document.getElementById('name')
+const date = document.getElementById('date')
+
+const animated_bgs = document.querySelectorAll('.animated-bg')
+const animated_bg_texts = document.querySelectorAll('.animated-bg-text')
+
+setTimeout(getData, 2500)
+
+function getData() {
+  header.innerHTML =
+    '<img src="https://fox5sandiego.com/wp-content/uploads/sites/15/2017/11/coco.jpg?w=1280" alt="" />'
+  title.innerHTML = 'Lorem ipsum dolor sit amet'
+  excerpt.innerHTML =
+    'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore perferendis'
+  profile_img.innerHTML =
+    '<img src="https://i.ytimg.com/vi/fBbQZS7lNk8/movieposter_en.jpg" alt="" />'
+  name.innerHTML = 'Coco'
+  date.innerHTML = 'Aug 11, 2021'
+
+  animated_bgs.forEach((bg) => bg.classList.remove('animated-bg'))
+  animated_bg_texts.forEach((bg) => bg.classList.remove('animated-bg-text'))
+}
